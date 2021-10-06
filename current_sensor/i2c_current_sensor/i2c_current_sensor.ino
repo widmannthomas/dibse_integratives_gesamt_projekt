19f253e2d260aa3cab5ebb39baa1132cea9f62a4
Thomas Widmann <mail@thomas-widmann.at>
2021-10-06 20:44:58 +0200

    bla

--- current_sensor/i2c_current_sensor/i2c_current_sensor.ino
+++ current_sensor/i2c_current_sensor/i2c_current_sensor.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
 #include <INA219.h>
 
+#define PIN_CTRL A1
+
 INA219 monitor;
 
 float shunt_r_ohm = 2.2;
@@ -19,20 +21,28 @@ void setup()
                     INA219::ADC_12BIT, // use ADC_2SAMP for more accurate sampling
                     INA219::CONT_SH);
   monitor.calibrate(shunt_r_ohm, 0.1);
+
+#ifdef PIN_CTRL
+  pinMode(PIN_CTRL, INPUT);
+#endif
 }
 
-void printCSV(float current_ma)
+void printCSV(float current_ma, int ctrl)
 {
   Serial.print(millis());
   Serial.print(" ");
-  Serial.println(current_ma, 5);
+  Serial.print(current_ma, 5);
+  Serial.print(" ");
+  Serial.println(ctrl);
 }
 
-void printForPlotter(float current_ma)
+void printForPlotter(float current_ma, int ctrl)
 {
   Serial.print(0);
   Serial.print(" ");
   Serial.print(140);
+  Serial.print(" ");
+  Serial.print(ctrl * 100);
   Serial.print(" ");
   Serial.println(current_ma, 5);
 }
@@ -41,7 +51,13 @@ void loop()
 {
   float voltage_mv = monitor.shuntVoltage() * 1000;
   float current_ma = voltage_mv / shunt_r_ohm;
+  int ctrl = true;
+
+#ifdef PIN_CTRL
+  ctrl = analogRead(PIN_CTRL) > 400;
+#endif
   
-  //printForPlotter(current_ma);
-  printCSV(current_ma);
+  
+  printForPlotter(current_ma, ctrl);
+  //printCSV(current_ma, ctrl);
 }
--- esp8266/tcp_udp/esp8266_tcp_udp/esp8266_tcp_udp.ino
+++ esp8266/tcp_udp/esp8266_tcp_udp/esp8266_tcp_udp.ino
@@ -4,15 +4,20 @@
 // Set WiFi credentials
 #define WIFI_SSID "***"
 #define WIFI_PASS "***"
-#define SERVER_IP "192.168.1.191"
+#define SERVER_IP "192.168.1.183"
 #define TCP_PORT 63477
 #define UDP_PORT 50262
+
+#define PIN_CTRL 2
 
 // UDP
 WiFiUDP UDP;
 
 void setup() 
 {
+  pinMode(PIN_CTRL, OUTPUT);
+  digitalWrite(PIN_CTRL, LOW);
+  
   // Setup serial port
   Serial.begin(115200);  
   WiFi.begin(WIFI_SSID, WIFI_PASS);
@@ -21,15 +26,20 @@ void setup()
   while (WiFi.status() != WL_CONNECTED){
     delay(100);
   }
+
+  Serial.println(WiFi.localIP());
+  delay(1000);
 }
 
 void loop() 
 { 
   Serial.println("Send UDP.");
 
+  digitalWrite(PIN_CTRL, HIGH);
+
   unsigned long unTime = millis();
 
-  /*
+  
   UDP.beginPacket(SERVER_IP, UDP_PORT);
   for(short n = 0; n < 10000; n++)
   {
@@ -37,13 +47,13 @@ void loop()
   }
   Serial.println("");
   UDP.endPacket();
+  UDP.flush();
+
+  delay(50);
 
   Serial.println("UDP took: " + String(millis() - unTime) + "ms");
-
-  delay(2000);
-  */
   
-
+  /*
   unTime = millis();
 
   Serial.println("Send TCP.");
@@ -58,6 +68,9 @@ void loop()
   Serial.println("");
 
   Serial.println("TCP took: " + String(millis() - unTime) + "ms");
+  */
 
-  delay(2000);
+  digitalWrite(PIN_CTRL, LOW);
+
+  delay(100);
 }
